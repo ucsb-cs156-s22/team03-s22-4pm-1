e3bf90d3067157fe567f342e0e2564e8830d86c9
root <haoyuanli@ucsb.edu>
2022-05-16 18:21:19 -0700

    started with it

---
+++ frontend/src/main/components/DiningCommonsMenuItems/DiningCommonsMenuItemsTable.js
@@ -0,0 +1,71 @@
+import OurTable, { ButtonColumn} from "main/components/OurTable";
+//import { useBackendMutation } from "main/utils/useBackend";
+//import {  onDeleteSuccess } from "main/utils/UCSBDateUtils"
+// import { useNavigate } from "react-router-dom";
+import { hasRole } from "main/utils/currentUser";
+
+
+// export function cellToAxiosParamsDelete(cell) {
+//     return {
+//         url: "/api/ucsbdiningcommons",
+//         method: "DELETE",
+//         params: {
+//             code: cell.row.values.code
+//         }
+//     }
+// }
+
+export default function DiningCommonsMenuItemsTable({ diningCommonsMenuItems, currentUser }) {
+
+    // const navigate = useNavigate();
+
+    // const editCallback = (cell) => {
+    //     navigate(`/ucsbdates/edit/${cell.row.values.id}`)
+    // }
+
+    // Stryker disable all : hard to test for query caching
+    // const deleteMutation = useBackendMutation(
+    //     cellToAxiosParamsDelete,
+    //     { onSuccess: onDeleteSuccess },
+    //     ["/api/ucsbdiningcommons/all"]
+    // );
+    // Stryker enable all 
+
+    // Stryker disable next-line all : TODO try to make a good test for this
+    // const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+
+    const columns = [
+        {
+            Header: 'DiningCommonsCode',
+            accessor: 'diningCommonsCode', 
+        },
+        {
+            Header: 'ID',
+            accessor: 'id',
+        },
+        {
+            Header: 'Name',
+            accessor: 'name',
+        },
+        {
+            Header: 'Station',
+            accessor: 'station',
+        }    
+    ];
+
+    //const testid = "DiningCommonsTable";
+
+    // const columnsIfAdmin = [
+    //     ...columns,
+    //     // ButtonColumn("Edit", "primary", editCallback, testid),
+    //     ButtonColumn("Delete", "danger", deleteCallback, testid)
+    // ];
+
+    const columnsToDisplay =  columns;
+
+    return <OurTable
+        data={diningCommonsMenuItems}
+        columns={columnsToDisplay}
+        testid={DiningCommonsMenuItemsTable}
+    />;
+};
