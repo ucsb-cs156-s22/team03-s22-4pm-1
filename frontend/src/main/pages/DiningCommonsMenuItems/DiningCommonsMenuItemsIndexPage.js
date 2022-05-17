5e655ec08255dbda91054af25d62e143c252d618
root <haoyuanli@ucsb.edu>
2022-05-16 17:23:59 -0700

    added placeholder page for items index

---
+++ frontend/src/main/pages/DiningCommonsMenuItems/DiningCommonsMenuItemsIndexPage.js
@@ -0,0 +1,14 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function DiningCommonsMenuItemsIndexPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Dining Commons Menu Items</h1>
+        <p>
+          This is where the index page will go
+        </p>
+      </div>
+    </BasicLayout>
+  )
+}
