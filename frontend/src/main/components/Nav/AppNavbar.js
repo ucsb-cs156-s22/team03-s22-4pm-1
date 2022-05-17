ccf42a9615a13f49436bdf4c0c07224ebd9c0975
root <haoyuanli@ucsb.edu>
2022-05-17 14:56:50 -0700

    changes on navbar

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -49,8 +49,8 @@ export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUr
               }
               {
                 hasRole(currentUser, "ROLE_USER") && (
-                  <NavDropdown title="Dining Commons Menu Items" id="appnavbar-dining-commons-menu-items-dropdown" data-testid="appnavbar-dining-commons-menu-items-dropdown" >
-                    <NavDropdown.Item as={Link} to="/diningCommonsMenuItems/list" data-testid ="appnavbar-dining-commons-menu-items-list">List Dining Commons Menu Items</NavDropdown.Item>
+                  <NavDropdown title="Dining Commons Menu Item" id="appnavbar-dining-commons-menu-item-dropdown" data-testid="appnavbar-dining-commons-menu-item-dropdown" >
+                    <NavDropdown.Item as={Link} to="/diningCommonsMenuItem/list" data-testid ="appnavbar-dining-commons-menu-item-list">List Dining Commons Menu Item</NavDropdown.Item>
                   </NavDropdown>
                 )
               }
