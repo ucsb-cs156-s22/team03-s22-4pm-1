ed021db49c2a7d8827bef9d2bb8617ea390f3534
root <haoyuanli@ucsb.edu>
2022-05-17 21:34:29 -0700

    add story.js and fixture

---
+++ frontend/src/fixtures/diningCommonsMenuItemFixtures.js
@@ -0,0 +1,31 @@
+const diningCommonsMenuItemFixtures = {
+    oneDiningCommonsMenuItem: {
+        "diningCommonsCode": "ortega",
+        "id": 1,
+        "name":"Chicken Caesar Salad" ,
+        "station": "Entrees"
+    },
+    threeDiningCommonsMenuItem: [
+        {
+            "diningCommonsCode": "ortega",
+            "id": 1,
+            "name":"Chicken Caesar Salad" ,
+            "station": "Entrees"
+        },
+        {
+            "diningCommonsCode": "carrilo",
+            "id": 2,
+            "name":"Chicken BBQ" ,
+            "station": "Entree Specials"
+        },
+        {
+            "diningCommonsCode": "portola",
+            "id": 3,
+            "name":"Beef Stew" ,
+            "station": "International"
+        } 
+    ]
+};
+
+
+export { diningCommonsMenuItemFixtures };
