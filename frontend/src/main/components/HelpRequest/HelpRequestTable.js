71b66ead78535b1dc2a696fbeb0a40478c646a9d
anwu0203 <andywu@umail.ucsb.edu>
2022-05-18 18:03:53 -0700

    aw - implemented and tested storybook, table, and delete button for Help Request

--- frontend/src/main/components/HelpRequest/HelpRequestTable.js
+++ frontend/src/main/components/HelpRequest/HelpRequestTable.js
@@ -1,10 +1,10 @@
-import OurTable from "main/components/OurTable";
-//import { useBackendMutation } from "main/utils/useBackend";
-//import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/UCSBDateUtils"
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+import { useBackendMutation } from "main/utils/useBackend";
+import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/HelpRequestUtils"
 //import { useNavigate } from "react-router-dom";
-//import { hasRole } from "main/utils/currentUser";
+import { hasRole } from "main/utils/currentUser";
 
-export default function HelpRequestTable({ helpRequest, _currentUser }) {
+export default function HelpRequestTable({ helpRequest, currentUser }) {
 
     //const navigate = useNavigate();
 
@@ -13,15 +13,15 @@ export default function HelpRequestTable({ helpRequest, _currentUser }) {
     // }
 
     // Stryker disable all : hard to test for query caching
-    // const deleteMutation = useBackendMutation(
-    //     cellToAxiosParamsDelete,
-    //     { onSuccess: onDeleteSuccess },
-    //     ["/api/ucsbdates/all"]
-    // );
+    const deleteMutation = useBackendMutation(
+        cellToAxiosParamsDelete,
+        { onSuccess: onDeleteSuccess },
+        ["/api/helprequest/all"]
+    );
     // Stryker enable all 
 
     // Stryker disable next-line all : TODO try to make a good test for this
-    //const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
 
     const columns = [
         {
@@ -55,15 +55,13 @@ export default function HelpRequestTable({ helpRequest, _currentUser }) {
         }
     ];
 
-    // const columnsIfAdmin = [
-    //     ...columns,
-    //     ButtonColumn("Edit", "primary", editCallback, "UCSBDatesTable"),
-    //     ButtonColumn("Delete", "danger", deleteCallback, "UCSBDatesTable")
-    // ];
+    const columnsIfAdmin = [
+        ...columns,
+        //ButtonColumn("Edit", "primary", editCallback, "UCSBDatesTable"),
+        ButtonColumn("Delete", "danger", deleteCallback, "HelpRequestTable")
+    ];
 
-    // const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
-
-    const columnsToDisplay = columns;
+    const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
 
 
     return <OurTable
